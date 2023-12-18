f836bd837aa64efcf581c9620029c769cd77ab72
TimothyMcCann <timothysmccann@gmail.com>
2023-12-18 14:54:17 -0500

    completed lecture

--- src/index.js
+++ src/index.js
@@ -26,7 +26,84 @@ function renderCharacters(charArr) {
         li.appendChild(p)
         li.appendChild(img)
 
-        ul.append(li)
+        //create update form
+
+        const form = document.createElement('form')
+        //console.log(form)
+        const btn = document.createElement('button')
+        const input = document.createElement('input')
+        input.placeholder = "character name"
+        input.name = "name"
+        btn.innerText = "Submit"
         
+        //console.log(input)
+        form.append(input, btn)
+        //if used appendChild would need to have made 2
+        li.appendChild(form)
+
+        form.addEventListener('submit', (e) => handleUpdateChar(e))
+
+        function handleUpdateChar(e){
+            e.preventDefault()
+
+            //console.log(e.target.name.value)
+
+            let newCharNameObj = {
+                name : e.target.name.value
+            }
+            console.log(e.target.name.value)
+
+            fetch(`http://localhost:3000/characters/${charObj.id}`,
+                {
+                    method : 'PATCH',
+                    headers : {
+                        'Content-type' : 'application/json'
+                    },
+                    body : JSON.stringify(newCharNameObj)
+                }
+            )
+            .then((resp) => resp.json())
+            .then((newCharObj) => renderCharacters(charArr.map((eachCharObj) => eachCharObj.id == charObj.id ? newCharObj : eachCharObj)))
+            // .then((newCharObj) => {
+            //     const newCharArr = charArr.map((eachCharObj) => {
+            //         if(eachCharObj.id == charObj.id){
+            //             return newCharObj
+            //         }
+            //         else {
+            //             return eachCharObj
+            //         }
+            //     })
+            //     renderCharacters(newCharArr)
+            // })
+        }
+
+        const btn2 = document.createElement('button')
+        btn2.textContent = "Delete"
+
+        li.appendChild(btn2)
+        //btn2.addEventListener('click', handleCharDelete)
+        btn2.addEventListener('click', (e) => handleCharDelete(e))
+
+        function handleCharDelete(e){
+            fetch(`http://localhost:3000/characters/${charObj.id}`,
+            {
+                method : 'DELETE'
+            })
+            .then((resp) => resp.json())
+            .then(() => renderCharacters(charArr.filter((eachCharObj) => eachCharObj.id == charObj.id ? false : true)))
+            // .then(() => {
+            //     const newCharArr = charArr.filter((eachCharObj) =>
+            //     {
+            //         if(eachCharObj.id == charObj.id){
+            //             return false
+            //         }else{
+            //             return true
+            //         }
+            //     })
+            //     renderCharacters(newCharArr)
+            // })
+        }
+
+        ul.append(li)
     })
 }
